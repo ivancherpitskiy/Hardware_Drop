1f5f152b541d02cc92abd1a3294522351463e290
ivancherpitskiy <ivancherpitskiy@gmail.com>
2026-08-24 09:17:27 +0300

    Delete storage_project directory

--- storage_project/display.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <stdbool.h>
-
-bool display_init(void);
-void display_clear(void);
-void display_print(int page, int col, const char* str);
-
-// Головний цикл відмальовки екрана
-void *display_thread_loop(void *arg);
